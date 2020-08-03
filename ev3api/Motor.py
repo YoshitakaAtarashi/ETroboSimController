771e339a7accb6fb6aedd20027e206df831b16c4
YoshitakaAtarashi <61078108+YoshitakaAtarashi@users.noreply.github.com>
2020-08-03 17:36:44 +0900

    引数に型を書くテスト

--- ev3api/Motor.py
+++ ev3api/Motor.py
@@ -12,7 +12,29 @@ class Motor:
     PWM_MAX = 100
     PWM_MIN = -100
 
-    def __init__(self, port, brake=True, type_=MotorType.LARGE_MOTOR):
+    def __init__(self, port, brake : bool = True, type_ : MotorType = MotorType.LARGE_MOTOR):
         self.port = port
         self.brake = brake
         self.type = type_
+        self.count = 0
+        self.offset = 0
+        self.pwm = 0
+
+    def reset(self):
+        self.brake= True 
+        self.count = 0   # ev3_motor_reset_counts(mPort);
+        self.offset = 0  
+
+    def getCount(self):
+        return self.count-self.offset
+
+    def setCount(self, count : int):
+        self.offset = self.count-count
+
+    def setPWM(self, pwm : int):
+        self.pwm = pwm 
+
+    def setBrake(self, brake : bool):
+        self.brake = brake
+    
+
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 from ev3api.Motor import *
 
 motor=Motor(1,True,MotorType.MEDIUM_MOTOR)
+print(motor.type)
+print(motor.getCount())
+
+motor.setBrake(True)
+
+
+motor=Motor(1,True,type_=MotorType.MEDIUM_MOTOR)
